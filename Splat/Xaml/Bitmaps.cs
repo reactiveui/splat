1f42e2149054882cb12c8003f5b44c6cf8cc86ae
Paul Betts <paul@github.com>
2013-06-30 15:57:21 -0700

    Update XAML Bitmaps for WP8 support

--- Splat/Xaml/Bitmaps.cs
+++ Splat/Xaml/Bitmaps.cs
@@ -14,25 +14,44 @@ namespace Splat
         public Task<IBitmap> Load(Stream sourceStream, float? desiredWidth, float? desiredHeight)
         {
             return Task.Run(() => {
-                var source = new BitmapImage();
+                var ret = new BitmapImage();
 
-                source.BeginInit();
-                source.StreamSource = sourceStream;
-                if (desiredWidth != null)
-                {
-                    source.DecodePixelWidth = (int)desiredWidth;
-                    source.DecodePixelHeight = (int)desiredHeight;
-                }
-                source.EndInit();
-                source.Freeze();
+                withInit(ret, source => {
+                    if (desiredWidth != null) {
+                        source.DecodePixelWidth = (int)desiredWidth;
+                        source.DecodePixelHeight = (int)desiredHeight;
+                    }
 
-                return (IBitmap) new BitmapSourceBitmap(source);
+#if SILVERLIGHT
+                    source.SetSource(sourceStream);
+#else
+                    source.StreamSource = sourceStream;
+#endif
+                });
+
+                return (IBitmap) new BitmapSourceBitmap(ret);
             });
         }
 
         public IBitmap Create(float width, float height)
         {
+#if SILVERLIGHT
+            return (IBitmap)new BitmapSourceBitmap(new WriteableBitmap((int)width, (int)height));
+#else
             return (IBitmap) new BitmapSourceBitmap(new WriteableBitmap((int)width, (int)height, 96, 96, PixelFormats.Default, null));
+#endif
+        }
+
+        void withInit(BitmapImage source, Action<BitmapImage> block)
+        {
+#if SILVERLIGHT
+            block(source);
+#else
+            source.BeginInit();
+            block(source);
+            source.EndInit();
+            source.Freeze();
+#endif
         }
     }
 
@@ -46,22 +65,35 @@ namespace Splat
         public BitmapSourceBitmap(BitmapSource bitmap)
         {
             inner = bitmap;
+#if SILVERLIGHT
+            Width = (float)inner.PixelWidth;
+            Height = (float)inner.PixelHeight;
+#else
             Width = (float)inner.Width;
             Height = (float)inner.Height;
+#endif
         }
 
         public Task Save(CompressedBitmapFormat format, float quality, Stream target)
         {
             return Task.Run(() => {
+#if SILVERLIGHT
+                if (format == CompressedBitmapFormat.Png) {
+                    throw new PlatformNotSupportedException("WP8 can't save PNGs.");
+                }
+
+                var wb = new WriteableBitmap(inner);
+                wb.SaveJpeg(target, wb.PixelWidth, wb.PixelHeight, 0, (int)(quality * 100.0f));
+#else
                 var encoder = format == CompressedBitmapFormat.Jpeg ?
                     (BitmapEncoder)new JpegBitmapEncoder() { QualityLevel = (int)(quality * 100.0f) } :
                     (BitmapEncoder)new PngBitmapEncoder();
 
                 encoder.Frames.Add(BitmapFrame.Create(inner));
                 encoder.Save(target);
+#endif
             });
         }
-
         public void Dispose()
         {
             inner = null;
