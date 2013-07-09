ef3bcd057bedaca5d604a1e53db082a0cc1459be
Paul Betts <paul@github.com>
2013-07-09 16:50:23 -0700

    Omgggggggg

--- Splat/Android/Color.cs
+++ Splat/Android/Color.cs
@@ -5,12 +5,12 @@ namespace Splat
 {
     public static class ColorExtensions
     {
-        public static Color ToNative(System.Drawing.Color This)
+        public static Color ToNative(this System.Drawing.Color This)
         {
             return new Color(This.R, This.G, This.B, This.A);
         }
 
-        public static System.Drawing.Color FromNative(Color This)
+        public static System.Drawing.Color FromNative(this Color This)
         {
             return System.Drawing.Color.FromArgb(This.A, This.R, This.G, This.B);
         }
