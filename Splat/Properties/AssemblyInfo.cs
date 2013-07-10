b68137b52f07523c11e4d52f3781e42c7f3af2f6
Paul Betts <paul@paulbetts.org>
2013-07-09 17:02:03 -0700

    Version bump to 0.2.0

--- Splat/Properties/AssemblyInfo.cs
+++ Splat/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.1")]
-[assembly: AssemblyFileVersion("0.1.1")]
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.2.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
