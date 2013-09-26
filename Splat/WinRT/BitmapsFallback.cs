886b87d1cc20ea5b301812c8232fcb79e620c8c0
Richard Z.H. Wang <zhwang@live.com.au>
2013-09-27 09:00:44 +1000

    Move COM interop stuff to its own file

--- Splat/WinRT/BitmapsFallback.cs
+++ Splat/WinRT/BitmapsFallback.cs
@@ -23,10 +23,10 @@ namespace Splat
             var frameSize = frame.GetSize();
 
             BitmapSource convertedSource;
-            if (!frame.GetPixelFormat().Equals(WICPixelFormat32bppBGRA))
+            if (!frame.GetPixelFormat().Equals(NativeMethods.WICPixelFormat32bppBGRA))
             {
                 FormatConverter converter = factory.CreateFormatConverter();
-                converter.Initialize(frame, WICPixelFormat32bppBGRA);
+                converter.Initialize(frame, NativeMethods.WICPixelFormat32bppBGRA);
                 convertedSource = converter;
             }
             else
@@ -52,66 +52,8 @@ namespace Splat
             throw new NotImplementedException();
         }
 
-
-        // understatement of the year: here be dragons
-
-
-        [Flags]
-        enum CLSCTX : uint
-        {
-            CLSCTX_INPROC_SERVER = 0x1,
-            CLSCTX_INPROC_HANDLER = 0x2,
-            CLSCTX_LOCAL_SERVER = 0x4,
-            CLSCTX_INPROC_SERVER16 = 0x8,
-            CLSCTX_REMOTE_SERVER = 0x10,
-            CLSCTX_INPROC_HANDLER16 = 0x20,
-            CLSCTX_RESERVED1 = 0x40,
-            CLSCTX_RESERVED2 = 0x80,
-            CLSCTX_RESERVED3 = 0x100,
-            CLSCTX_RESERVED4 = 0x200,
-            CLSCTX_NO_CODE_DOWNLOAD = 0x400,
-            CLSCTX_RESERVED5 = 0x800,
-            CLSCTX_NO_CUSTOM_MARSHAL = 0x1000,
-            CLSCTX_ENABLE_CODE_DOWNLOAD = 0x2000,
-            CLSCTX_NO_FAILURE_LOG = 0x4000,
-            CLSCTX_DISABLE_AAA = 0x8000,
-            CLSCTX_ENABLE_AAA = 0x10000,
-            CLSCTX_FROM_DEFAULT_CONTEXT = 0x20000,
-            CLSCTX_ACTIVATE_32_BIT_SERVER = 0x40000,
-            CLSCTX_ACTIVATE_64_BIT_SERVER = 0x80000,
-            CLSCTX_INPROC = CLSCTX_INPROC_SERVER | CLSCTX_INPROC_HANDLER,
-            CLSCTX_SERVER = CLSCTX_INPROC_SERVER | CLSCTX_LOCAL_SERVER | CLSCTX_REMOTE_SERVER,
-            CLSCTX_ALL = CLSCTX_SERVER | CLSCTX_INPROC_HANDLER
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        struct MultiQueryInterface
-        {
-            public IntPtr InterfaceIID;
-            public IntPtr IUnknownPointer;
-            public int ResultCode;
-        }
-
-        enum WICDecodeOptions : uint
-        {
-            WICDecodeMetadataCacheOnDemand = 0,
-            WICDecodeMetadataCacheOnLoad = 1,
-            WICMETADATACACHEOPTION_FORCE_DWORD = 0x7fffffff
-        }
-
-        const int S_OK = 0;
-
-        static readonly Guid WICPixelFormat1bppIndexed = new Guid("6fddc324-4e03-4bfe-b185-3d77768dc901");
-        static readonly Guid WICPixelFormatBlackWhite = new Guid("6fddc324-4e03-4bfe-b185-3d77768dc905");
-        static readonly Guid WICPixelFormat8bppIndexed = new Guid("6fddc324-4e03-4bfe-b185-3d77768dc904");
-        static readonly Guid WICPixelFormatDontCare = new Guid("6fddc324-4e03-4bfe-b185-3d77768dc900");
-        static readonly Guid WICPixelFormat24bppBGR = new Guid("6fddc324-4e03-4bfe-b185-3d77768dc90c");
-        static readonly Guid WICPixelFormat32bppBGRA = new Guid("6fddc324-4e03-4bfe-b185-3d77768dc90e");
-        static readonly Guid WICPixelFormat48bppRGB = new Guid("6fddc324-4e03-4bfe-b185-3d77768dc915");
-
         class ImagingFactory
         {
-            static readonly Guid CLSID_WICImagingFactory = new Guid("cacaf262-9370-4615-a13b-9f5539da4c0a");
 
             IWICImagingFactory comObject;
             IntPtr nativePointer;
@@ -129,8 +71,8 @@ namespace Splat
                     IUnknownPointer = IntPtr.Zero,
                     ResultCode = 0,
                 };
-                var result = CoCreateInstanceFromApp(CLSID_WICImagingFactory, IntPtr.Zero, CLSCTX.CLSCTX_INPROC_SERVER, IntPtr.Zero, 1, ref localQuery);
-                if (result != S_OK || localQuery.ResultCode != S_OK)
+                var result = NativeMethods.CoCreateInstanceFromApp(NativeMethods.CLSID_WICImagingFactory, IntPtr.Zero, CLSCTX.CLSCTX_INPROC_SERVER, IntPtr.Zero, 1, ref localQuery);
+                if (result != NativeMethods.S_OK || localQuery.ResultCode != NativeMethods.S_OK)
                     throw new Exception("CoCreateInstanceFromApp failed");
                 this.nativePointer = localQuery.IUnknownPointer;
                 this.comObject = (IWICImagingFactory)Marshal.GetObjectForIUnknown(nativePointer);
@@ -147,7 +89,7 @@ namespace Splat
                 IntPtr nativePointer;
                 Guid nullGuid = Guid.Empty;
                 var result = comObject.CreateDecoderFromStream(new ManagedIStream(stream), ref nullGuid, wicDecodeOptions, out nativePointer);
-                if (result != S_OK)
+                if (result != NativeMethods.S_OK)
                     throw new Exception("CreateDecoderFromStream failed");
                 return new BitmapDecoder(nativePointer);
             }
@@ -156,37 +98,9 @@ namespace Splat
             {
                 IntPtr nativePointer;
                 var result = comObject.CreateFormatConverter(out nativePointer);
-                if (result != S_OK)
+                if (result != NativeMethods.S_OK)
                     throw new Exception("CreateFormatConverter failed");
                 return new FormatConverter(nativePointer);
-            }
-
-            [DllImport("ole32.dll", ExactSpelling = true, EntryPoint = "CoCreateInstanceFromApp", PreserveSig = true)]
-            private static extern int CoCreateInstanceFromApp(
-                [In, MarshalAs(UnmanagedType.LPStruct)] Guid rclsid,
-                IntPtr pUnkOuter,
-                CLSCTX dwClsContext,
-                IntPtr reserved,
-                int countMultiQuery,
-                ref MultiQueryInterface query);
-
-            [Guid("ec5ec8a9-c395-4314-9c77-54d7a935ff70")]
-            [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-            [ComImport]
-            interface IWICImagingFactory
-            {
-                void CreateDecoderFromFilenameDummy();
-                int CreateDecoderFromStream(
-                    IStream pIStream,
-                    ref Guid pguidVendor,
-                    WICDecodeOptions metadataOptions,
-                    out IntPtr ppIDecoder);
-                void CreateDecoderFromFileHandleDummy();
-                void CreateComponentInfoDummy();
-                void CreateDecoderDummy();
-                void CreateEncoderDummy();
-                void CreatePaletteDummy();
-                int CreateFormatConverter(out IntPtr ppIFormatConverter);
             }
         }
 
@@ -205,27 +119,9 @@ namespace Splat
             {
                 IntPtr nativePointer;
                 var result = comObject.GetFrame(index, out nativePointer);
-                if (result != S_OK)
+                if (result != NativeMethods.S_OK)
                     throw new Exception("GetFrame failed");
                 return new BitmapFrameDecode(nativePointer);
-            }
-
-            [Guid("9EDDE9E7-8DEE-47ea-99DF-E6FAF2ED44BF")]
-            [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-            [ComImport]
-            interface IWICBitmapDecoder
-            {
-                void QueryCapabilityDummy();
-                void InitializeDummy();
-                void GetContainerFormatDummy();
-                void GetDecoderInfoDummy();
-                void CopyPaletteDummy();
-                void GetMetadataQueryReaderDummy();
-                void GetPreviewDummy();
-                void GetColorContextsDummy();
-                void GetThumbnailDummy();
-                void GetFrameCountDummy();
-                int GetFrame(uint index, out IntPtr ppIFrameDecode);
             }
         }
 
@@ -277,22 +173,6 @@ namespace Splat
             {
                 comObject.GetSize(out width, out height);
             }
-
-            [Guid("00000120-a8f2-4877-ba0a-fd2b6645fb94")]
-            [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-            [ComImport]
-            public interface IWICBitmapSource
-            {
-                void GetSize(out uint puiWidth, out uint puiHeight);
-                void GetPixelFormat(out Guid pPixelFormat);
-                void GetResolutionDummy();
-                void CopyPaletteDummy();
-                void CopyPixels(
-                    IntPtr prc, // WICRect
-                    uint cbStride,
-                    uint cbBufferSize,
-                    IntPtr pbBuffer);
-            }
         }
 
         class BitmapFrameDecode : BitmapSource
@@ -314,28 +194,6 @@ namespace Splat
             {
                 comObject.Initialize(bitmapSource.NativePointer, pixelFormat, 0, IntPtr.Zero, 0.0f, 0);
             }
-
-            [Guid("00000301-a8f2-4877-ba0a-fd2b6645fb94")]
-            [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-            [ComImport]
-            public interface IWICFormatConverter
-            {
-                #region IWICBitmapSource
-                void GetSizeDummy();
-                void GetPixelFormatDummy();
-                void GetResolutionDummy();
-                void CopyPaletteDummy();
-                void CopyPixelsDummy();
-                #endregion
-                void Initialize(
-                    IntPtr pISource,
-                    [MarshalAs(UnmanagedType.LPStruct)]
-                    Guid dstFormat,
-                    uint dither,
-                    IntPtr pIPalette,
-                    double alphaThresholdPercent,
-                    uint paletteTranslate);
-            };
         }
 
         class ManagedIStream : IStream
