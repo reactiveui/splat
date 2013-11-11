5648a814490afa1c159092551816bf19add67d3a
Paul Betts <paul@paulbetts.org>
2013-11-10 20:35:51 -0800

    Enable unifying Splat Portable types with built-in ones

---
+++ Splat/TypeForwardedSystemDrawing.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(Color))]
+[assembly: TypeForwardedTo(typeof(Point))]
+[assembly: TypeForwardedTo(typeof(PointF))]
+[assembly: TypeForwardedTo(typeof(Rectangle))]
+[assembly: TypeForwardedTo(typeof(RectangleF))]
+[assembly: TypeForwardedTo(typeof(Size))]
+[assembly: TypeForwardedTo(typeof(SizeF))]
