976c8f147fbb890fa3ded4410f4e42caa9307756
Paul Betts <paul@paulbetts.org>
2014-04-07 11:44:37 -0700

    Remove misleading comment (that is also incorrect)

--- Splat/Cocoa/PlatformRegistrations.cs
+++ Splat/Cocoa/PlatformRegistrations.cs
@@ -6,8 +6,6 @@ using System.Threading.Tasks;
 
 namespace Splat
 {
-    // NB: This code isn't included in any of the platform-specific libraries
-    // it's just to give the PCL something to link against
     static class PlatformRegistrations
     {
         internal static void Register(IMutableDependencyResolver This)
