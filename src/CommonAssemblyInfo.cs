3e156a385871bf8a779ac577c1813f1cd489c7e6
Geoffrey Huntley <ghuntley@ghuntley.com>
2016-10-24 23:19:54 +1100

    retargeted bait to netstandard 1.1 and resolved packaging

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Reflection;
 [assembly: AssemblyProduct("Splat")]
 [assembly: AssemblyVersion("0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0")]
-[assembly: AssemblyCopyright("Copyright (c) Paul Betts")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
+[assembly: AssemblyCopyright("Copyright (c) Splat Contributors")]
 
