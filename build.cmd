8e3b2fb7c2bd2129849c7b9b9070f513ce9629a9
Geoffrey Huntley <ghuntley@ghuntley.com>
2016-10-24 22:17:15 +1100

    I prefer and find verbose output on this script useful

--- build.cmd
+++ build.cmd
@@ -1,8 +1,8 @@
 @echo off
-tools\nuget\nuget.exe update -self -verbosity quiet
-tools\nuget\nuget.exe install xunit.runner.console -OutputDirectory tools -ExcludeVersion -verbosity quiet
-tools\nuget\nuget.exe install Cake -OutputDirectory tools -ExcludeVersion -verbosity quiet
+tools\nuget\nuget.exe update -self
+tools\nuget\nuget.exe install xunit.runner.console -OutputDirectory tools -ExcludeVersion
+tools\nuget\nuget.exe install Cake -OutputDirectory tools -ExcludeVersion
 
-tools\Cake\Cake.exe build.cake --target=%1
+tools\Cake\Cake.exe build.cake
 
 exit /b %errorlevel%
