4f6354756bb905d426875cd501841279700c62fa
Geoffrey Huntley <ghuntley@ghuntley.com>
2016-10-24 22:14:39 +1100

    use seperate projects instead of project.json
    I know this strategy works, don't know of others whom have successfully gone this many platforms with project.json and dotnet cli. There also seems to be a unresolved blocking issue:
    
    https://github.com/dotnet/cli/issues/589#issuecomment-235671884

--- src/Splat/Properties/AssemblyInfo.cs
+++ src/Splat/Properties/AssemblyInfo.cs
@@ -3,24 +3,4 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Resources;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Splat")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
