c9490e868fd9d248f2c6fd1548b0bdd86292b1d0
Geoffrey Huntley <ghuntley@ghuntley.com>
2018-03-12 11:17:46 -0700

    housekeeping: upgrade to vs2017 project format, drop old platforms, make netstandard20 work (#159)

---
+++ src/Splat/Platforms/Cocoa/Bitmaps.cs
@@ -0,0 +1,136 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+#if UIKIT 
+using UIKit;
+using Foundation;
+#else
+using AppKit;
+using Foundation;
+
+using UIImage = AppKit.NSImage;
+using UIApplication = AppKit.NSApplication;
+#endif
+
+namespace Splat
+{
+    public class PlatformBitmapLoader : IBitmapLoader
+    {
+        public Task<IBitmap> Load(Stream sourceStream, float? desiredWidth, float? desiredHeight)
+        {
+            var data = NSData.FromStream(sourceStream);
+
+            var tcs = new TaskCompletionSource<IBitmap>();
+            UIApplication.SharedApplication.InvokeOnMainThread(() => {
+                try {
+#if UIKIT
+                    var bitmap = UIImage.LoadFromData(data);
+                    if (bitmap == null) {
+                        throw new Exception("Failed to load image");
+                    }
+
+                    tcs.TrySetResult(new CocoaBitmap(bitmap));
+#else
+                    tcs.TrySetResult(new CocoaBitmap(new UIImage(data)));
+#endif
+                } catch (Exception ex) {
+                    tcs.TrySetException(ex);
+                }
+            });
+            return tcs.Task;
+        }
+
+        public Task<IBitmap> LoadFromResource(string source, float? desiredWidth, float? desiredHeight)
+        {
+            var tcs = new TaskCompletionSource<IBitmap>();
+            UIApplication.SharedApplication.InvokeOnMainThread(() => {
+                try {
+#if UIKIT
+                    var bitmap = UIImage.FromBundle(source);
+#else
+                    var bitmap = UIImage.ImageNamed(source);
+#endif
+                    if (bitmap == null) {
+                        throw new Exception("Failed to load image from resource: " + source);
+                    }
+
+                    tcs.TrySetResult(new CocoaBitmap(bitmap));
+                } catch (Exception ex) {
+                    tcs.TrySetException(ex);
+                }
+            });
+            return tcs.Task;
+        }
+
+        public IBitmap Create(float width, float height)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    sealed class CocoaBitmap : IBitmap
+    {
+        internal UIImage inner;
+        public CocoaBitmap(UIImage inner)
+        {
+            this.inner = inner;
+        }
+
+        public float Width {
+            get { return (float)inner.Size.Width; }
+        }
+
+        public float Height {
+            get { return (float)inner.Size.Height; }
+        }
+
+        public Task Save(CompressedBitmapFormat format, float quality, Stream target)
+        {
+            return Task.Run(() => {
+#if UIKIT
+                var data = format == CompressedBitmapFormat.Jpeg ? inner.AsJPEG((float)quality) : inner.AsPNG();
+                data.AsStream().CopyTo(target);
+
+#else
+
+                var rect = new CoreGraphics.CGRect();
+
+                var cgImage = inner.AsCGImage(ref rect, null, null);
+                var imageRep = new NSBitmapImageRep(cgImage);
+
+                var props = format == CompressedBitmapFormat.Png ? 
+                    new NSDictionary() : 
+                    new NSDictionary(new NSNumber(quality), new NSString("NSImageCompressionFactor"));
+
+                var type = format == CompressedBitmapFormat.Png ? NSBitmapImageFileType.Png : NSBitmapImageFileType.Jpeg;
+
+                var outData = imageRep.RepresentationUsingTypeProperties(type, props);
+                outData.AsStream().CopyTo(target);
+#endif
+            });
+        }
+
+        public void Dispose()
+        {
+            var disp = Interlocked.Exchange(ref inner, null);
+            if (disp != null) disp.Dispose();
+        }
+    }
+
+    public static class BitmapMixins
+    {
+        public static UIImage ToNative(this IBitmap This)
+        {
+            return ((CocoaBitmap)This).inner;
+        }
+
+        public static IBitmap FromNative(this UIImage This, bool copy = false)
+        {
+            if (copy) return new CocoaBitmap((UIImage)This.Copy());
+
+            return new CocoaBitmap(This);
+        }
+    }
+}
