0441b8c8244bb474e00c1dd0fdfaf042e221de6b
Colt <cab452005@gmail.com>
2018-08-20 16:24:22 +0900

    fix: UWP Color and Bitmap ToNative doesn't work (#202)
    - Re-add the WinRt Color and Bitmap extensions

---
+++ src/Splat/Platforms/WinRT/Color.cs
@@ -0,0 +1,41 @@
+﻿using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace Splat
+{
+    public static class ColorExtensions
+    {
+        public static Color ToNative(this System.Drawing.Color This)
+        {
+            return Color.FromArgb(This.A, This.R, This.G, This.B);
+        }
+
+        public static SolidColorBrush ToNativeBrush(this System.Drawing.Color This)
+        {
+            return new SolidColorBrush(This.ToNative());
+        }
+
+        public static System.Drawing.Color FromNative(this Color This)
+        {
+            return System.Drawing.Color.FromArgb(This.A, This.R, This.G, This.B);
+        }
+    }
+
+    public static class SplatColorExtensions
+    {
+        public static Color ToNative(this SplatColor This)
+        {
+            return Color.FromArgb(This.A, This.R, This.G, This.B);
+        }
+
+        public static SolidColorBrush ToNativeBrush(this SplatColor This)
+        {
+            return new SolidColorBrush(This.ToNative());
+        }
+
+        public static SplatColor FromNative(this Color This)
+        {
+            return SplatColor.FromArgb(This.A, This.R, This.G, This.B);
+        }
+    }
+}
