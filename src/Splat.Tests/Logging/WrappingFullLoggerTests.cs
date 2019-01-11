959afa07050146c2d76fce1c88f8576b10a59b47
Glenn Watson <glenn@glennwatson.net>
2019-01-10 23:14:43 -0800

    feature: add ActionLogger which takes a user given action

--- src/Splat.Tests/Logging/WrappingFullLoggerTests.cs
+++ src/Splat.Tests/Logging/WrappingFullLoggerTests.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -6,7 +11,7 @@ using System.Text;
 using Splat.Tests.Mocks;
 using Xunit;
 
-namespace Splat.Tests
+namespace Splat.Tests.Logging
 {
     /// <summary>
     /// Tests that verify the wrapping full logger is working.
--- src/Splat.Tests/Logging/WrappingPrefixLoggerTests.cs
+++ src/Splat.Tests/Logging/WrappingPrefixLoggerTests.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Splat.Tests.Mocks;
 using Xunit;
 
-namespace Splat.Tests
+namespace Splat.Tests.Logging
 {
     /// <summary>
     /// Tests the <see cref="WrappingPrefixLogger"/> class.
