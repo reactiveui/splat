032c73624cdc8947af8264fabac16692229296aa
Rodney Littles II <rodney.littles@gmail.com>
2019-01-20 00:21:32 -0600

    feature: Added Splat.Autofac (#234)
    - Added a AutofacDependencyResolver adapter
    - Added unit tests
    - Added Splat.Autofac to build.cake

---
+++ src/Splat.Autofac.Tests/DependencyResolverTests.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using Autofac;
+using ReactiveUI;
+using Shouldly;
+using Xunit;
+
+namespace Splat.Autofac.Tests
+{
+    /// <summary>
+    /// Tests to show the <see cref="AutofacDependencyResolver"/> works correctly.
+    /// </summary>
+    public class DependencyResolverTests
+    {
+        /// <summary>
+        /// Shoulds the resolve views.
+        /// </summary>
+        [Fact]
+        public void AutofacDependencyResolver_Should_Resolve_Views()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ViewOne>().As<IViewFor<ViewModelOne>>();
+            builder.RegisterType<ViewTwo>().As<IViewFor<ViewModelTwo>>();
+            builder.Build().UseAutofacDependencyResolver();
+
+            var viewOne = Locator.Current.GetService(typeof(IViewFor<ViewModelOne>));
+            var viewTwo = Locator.Current.GetService(typeof(IViewFor<ViewModelTwo>));
+
+            viewOne.ShouldNotBeNull();
+            viewOne.ShouldBeOfType<ViewOne>();
+            viewTwo.ShouldNotBeNull();
+            viewTwo.ShouldBeOfType<ViewTwo>();
+        }
+
+        /// <summary>
+        /// Shoulds the resolve views.
+        /// </summary>
+        [Fact]
+        public void AutofacDependencyResolver_Should_Resolve_Named_View()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ViewTwo>().Named<IViewFor<ViewModelTwo>>("Other");
+            builder.Build().UseAutofacDependencyResolver();
+
+            var viewTwo = Locator.Current.GetService(typeof(IViewFor<ViewModelTwo>), "Other");
+
+            viewTwo.ShouldNotBeNull();
+            viewTwo.ShouldBeOfType<ViewTwo>();
+        }
+
+        /// <summary>
+        /// Shoulds the resolve view models.
+        /// </summary>
+        [Fact]
+        public void AutofacDependencyResolver_Should_Resolve_View_Models()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ViewModelOne>().AsSelf();
+            builder.RegisterType<ViewModelTwo>().AsSelf();
+            builder.Build().UseAutofacDependencyResolver();
+
+            var vmOne = Locator.Current.GetService<ViewModelOne>();
+            var vmTwo = Locator.Current.GetService<ViewModelTwo>();
+
+            vmOne.ShouldNotBeNull();
+            vmTwo.ShouldNotBeNull();
+        }
+
+        /// <summary>
+        /// Shoulds the resolve screen.
+        /// </summary>
+        [Fact]
+        public void AutofacDependencyResolver_Should_Resolve_Screen()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<MockScreen>().As<IScreen>().SingleInstance();
+            builder.Build().UseAutofacDependencyResolver();
+
+            var screen = Locator.Current.GetService<IScreen>();
+
+            screen.ShouldNotBeNull();
+            screen.ShouldBeOfType<MockScreen>();
+        }
+    }
+}
