4e6d5d5d20482ccae70d75678a66d87ed08819ec
David Vreony <dpvreony@users.noreply.github.com>
2019-03-01 20:37:37 +0000

    initial import of app insights and exceptionless

---
+++ src/Splat.Exceptionless/ExceptionlessSplatLogger.cs
@@ -0,0 +1,160 @@
+﻿// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using Exceptionless;
+
+namespace Splat.Exceptionless
+{
+    /// <summary>
+    /// Exceptionless Logger into Splat.
+    /// </summary>
+    [DebuggerDisplay("Name={_sourceType} Level={Level}")]
+    public sealed class ExceptionlessSplatLogger : ILogger
+    {
+        private static readonly KeyValuePair<LogLevel, global::Exceptionless.Logging.LogLevel>[] _mappings = new[]
+        {
+            new KeyValuePair<LogLevel, global::Exceptionless.Logging.LogLevel>(LogLevel.Debug, global::Exceptionless.Logging.LogLevel.Debug),
+            new KeyValuePair<LogLevel, global::Exceptionless.Logging.LogLevel>(LogLevel.Info, global::Exceptionless.Logging.LogLevel.Info),
+            new KeyValuePair<LogLevel, global::Exceptionless.Logging.LogLevel>(LogLevel.Warn, global::Exceptionless.Logging.LogLevel.Warn),
+            new KeyValuePair<LogLevel, global::Exceptionless.Logging.LogLevel>(LogLevel.Error, global::Exceptionless.Logging.LogLevel.Error),
+            new KeyValuePair<LogLevel, global::Exceptionless.Logging.LogLevel>(LogLevel.Fatal, global::Exceptionless.Logging.LogLevel.Error)
+        };
+
+        private static readonly ImmutableDictionary<LogLevel, global::Exceptionless.Logging.LogLevel> _mappingsDictionary = _mappings.ToImmutableDictionary();
+
+        private readonly string _sourceType;
+        private readonly ExceptionlessClient _exceptionlessClient;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExceptionlessSplatLogger"/> class.
+        /// </summary>
+        /// <param name="sourceType">The type being tracked.</param>
+        /// <param name="exceptionlessClient">The exceptionless client instance to use.</param>
+        public ExceptionlessSplatLogger(
+            Type sourceType,
+            ExceptionlessClient exceptionlessClient)
+        {
+            _sourceType = sourceType.FullName;
+            _exceptionlessClient = exceptionlessClient ?? throw new ArgumentNullException(nameof(exceptionlessClient));
+            _exceptionlessClient.Configuration.Changed += OnInnerLoggerReconfigured;
+        }
+
+        /// <inheritdoc />
+        public LogLevel Level { get; private set; }
+
+        /// <inheritdoc />
+        public void Write(string message, LogLevel logLevel)
+        {
+            if ((int)logLevel < (int)Level)
+            {
+                return;
+            }
+
+            CreateLog(message, _mappingsDictionary[logLevel]);
+        }
+
+        /// <inheritdoc />
+        public void Write(Exception exception, string message, LogLevel logLevel)
+        {
+            if ((int)logLevel < (int)Level)
+            {
+                return;
+            }
+
+            CreateLog(exception, message, _mappingsDictionary[logLevel]);
+        }
+
+        /// <inheritdoc />
+        public void Write(string message, Type type, LogLevel logLevel)
+        {
+            if ((int)logLevel < (int)Level)
+            {
+                return;
+            }
+
+            CreateLog($"{type.Name}: {message}", _mappingsDictionary[logLevel]);
+        }
+
+        /// <inheritdoc />
+        public void Write(Exception exception, string message, Type type, LogLevel logLevel)
+        {
+            if ((int)logLevel < (int)Level)
+            {
+                return;
+            }
+
+            CreateLog(exception, $"{type.Name}: {message}", _mappingsDictionary[logLevel]);
+        }
+
+        private void CreateLog(string message, global::Exceptionless.Logging.LogLevel level)
+        {
+            _exceptionlessClient.SubmitLog(_sourceType, message, level);
+        }
+
+        private void CreateLog(Exception exception, string message, global::Exceptionless.Logging.LogLevel level)
+        {
+            var reference = Guid.NewGuid();
+
+            // exception is the parent
+            var eventBuilder = ExceptionlessClient.Default.CreateException(exception);
+            eventBuilder.SetReferenceId(reference.ToString());
+            eventBuilder.Submit();
+
+            // log event is the child
+            var logger = ExceptionlessClient.Default.CreateLog(
+                _sourceType,
+                message,
+                level);
+            logger.SetEventReference("ReferenceId", reference.ToString());
+            logger.Submit();
+        }
+
+        /// <summary>
+        /// Works out the log level.
+        /// </summary>
+        /// <remarks>
+        /// This was done so the Level property doesn't keep getting re-evaluated each time a Write method is called.
+        /// </remarks>
+        private void SetLogLevel()
+        {
+            /*
+            if (_inner.IsDebugEnabled)
+            {
+                Level = LogLevel.Debug;
+                return;
+            }
+
+            if (_inner.IsInfoEnabled)
+            {
+                Level = LogLevel.Info;
+                return;
+            }
+
+            if (_inner.IsWarnEnabled)
+            {
+                Level = LogLevel.Warn;
+                return;
+            }
+
+            if (_inner.IsErrorEnabled)
+            {
+                Level = LogLevel.Error;
+                return;
+            }
+            */
+
+            Level = LogLevel.Fatal;
+        }
+
+        private void OnInnerLoggerReconfigured(object sender, EventArgs e)
+        {
+            SetLogLevel();
+        }
+    }
+}
---
+++ src/Splat.Exceptionless/MutableDependencyResolverExtensions.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using Exceptionless;
+
+namespace Splat.Exceptionless
+{
+    /// <summary>
+    /// Exceptionless specific extensions for the Mutable Dependency Resolver.
+    /// </summary>
+    public static class MutableDependencyResolverExtensions
+    {
+        /// <summary>
+        /// Simple helper to initialize NLog within Splat with the Wrapping Full Logger.
+        /// </summary>
+        /// <remarks>
+        /// You should configure NLog prior to calling this method.
+        /// </remarks>
+        /// <param name="instance">
+        /// An instance of Mutable Dependency Resolver.
+        /// </param>
+        /// <param name="exceptionlessClient">The exceptionless client instance to use.</param>
+        /// <example>
+        /// <code>
+        /// Locator.CurrentMutable.UseExceptionlessWithWrappingFullLogger(exception);
+        /// </code>
+        /// </example>
+        public static void UseExceptionlessWithWrappingFullLogger(this IMutableDependencyResolver instance, ExceptionlessClient exceptionlessClient)
+        {
+            var funcLogManager = new FuncLogManager(type =>
+            {
+                var miniLoggingWrapper = new ExceptionlessSplatLogger(type, exceptionlessClient);
+                return new WrappingFullLogger(miniLoggingWrapper);
+            });
+
+            instance.RegisterConstant(funcLogManager, typeof(ILogManager));
+        }
+    }
+}
