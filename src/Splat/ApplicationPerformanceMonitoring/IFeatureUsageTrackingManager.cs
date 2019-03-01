52d5b5320343e885e8d11c306871d5c5741895ae
David Vreony <dpvreony@users.noreply.github.com>
2019-03-01 19:33:54 +0000

    initial apm logic from the rfc post

---
+++ src/Splat/ApplicationPerformanceMonitoring/IFeatureUsageTrackingManager.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Splat.ApplicationPerformanceMonitoring
+{
+    /// <summary>
+    /// A manager which will generate a <see cref="IFeatureUsageTrackingSession"/> for the specified type.
+    /// </summary>
+    public interface IFeatureUsageTrackingManager
+    {
+        /// <summary>
+        /// Generate a <see cref="IFeatureUsageTrackingSession"/> for the specified type.
+        /// </summary>
+        /// <param name="type">The type to generate the feature usage tracking session for.</param>
+        /// <returns>The <see cref="IFeatureUsageTrackingSession"/> for the specified type.</returns>
+        IFeatureUsageTrackingSession GetFeatureUsageTrackingSession(Type type);
+    }
+}
