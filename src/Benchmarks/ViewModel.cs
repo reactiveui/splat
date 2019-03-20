24069679fe92866cac02117ecd5188e8a37ec33c
Glenn Watson <GitHub@glennwatson.net>
2019-03-20 00:40:20 -0500

    housekeeping: Update to match master at 48fabe2087a21b11d9cdc1a19227888229239aa1

---
+++ src/Benchmarks/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+namespace Splat.Benchmarks
+{
+    /// <summary>
+    /// A plain object for registration.
+    /// </summary>
+    public class ViewModel
+    {
+    }
+}
