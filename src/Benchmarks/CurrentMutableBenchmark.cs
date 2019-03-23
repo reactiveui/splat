0a4081a666f23e465037b792b719fa9d30ea1d34
Rodney Littles, II <6969701+RLittlesII@users.noreply.github.com>
2019-03-22 21:17:24 -0500

    performance: Added Benchmarks

---
+++ src/Benchmarks/CurrentMutableBenchmark.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace Splat.Benchmarks
+{
+    /// <summary>
+    /// Benchmarks for the current mutable locator.
+    /// </summary>
+    [ClrJob]
+    [CoreJob]
+    [MemoryDiagnoser]
+    [MarkdownExporterAttribute.GitHub]
+    public class CurrentMutableBenchmark
+    {
+        /// <summary>
+        /// Setup method for when running all bench marks.
+        /// </summary>
+        [GlobalSetup]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Benchamrks registering an object.
+        /// </summary>
+        [Benchmark]
+        public void Register() => Locator.CurrentMutable.Register(() => new ViewModel());
+
+        /// <summary>
+        /// Benchamrks registering an object.
+        /// </summary>
+        [Benchmark]
+        public void RegisterWithContract() => Locator.CurrentMutable.Register(() => new ViewModel(), nameof(ViewModel));
+
+        /// <summary>
+        /// Benchamrks registering an object.
+        /// </summary>
+        [Benchmark]
+        public void RegisterConstant() => Locator.CurrentMutable.RegisterConstant(new ViewModel());
+
+        /// <summary>
+        /// Benchamrks registering an object.
+        /// </summary>
+        [Benchmark]
+        public void RegisterConstantWithContract() => Locator.CurrentMutable.RegisterConstant(new ViewModel(), nameof(ViewModel));
+
+        /// <summary>
+        /// Benchamrks registering an object.
+        /// </summary>
+        [Benchmark]
+        public void RegisterLazySingleton() => Locator.CurrentMutable.RegisterLazySingleton(() => new ViewModel());
+
+        /// <summary>
+        /// Benchamrks registering an object.
+        /// </summary>
+        [Benchmark]
+        public void RegisterLazySingletonWithContract() => Locator.CurrentMutable.RegisterLazySingleton(() => new ViewModel(), nameof(ViewModel));
+    }
+}
---
+++ src/Benchmarks/DependencyResolver.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splat.Benchmarks
+{
+#pragma warning disable CA1001, CA1063
+    /// <summary>
+    /// <see cref="IDependencyResolver"/> implementation for benchmarking the Locator.
+    /// </summary>
+    /// <seealso cref="Splat.IDependencyResolver" />
+    public class DependencyResolver : IDependencyResolver
+    {
+        /// <inheritdoc />
+        public object GetService(Type serviceType, string contract = null) => default(object);
+
+        /// <inheritdoc />
+        public IEnumerable<object> GetServices(Type serviceType, string contract = null) => Enumerable.Empty<object>();
+
+        /// <inheritdoc />
+        public void Register(Func<object> factory, Type serviceType, string contract = null)
+        {
+        }
+
+        /// <inheritdoc />
+        public void UnregisterCurrent(Type serviceType, string contract = null)
+        {
+        }
+
+        /// <inheritdoc />
+        public void UnregisterAll(Type serviceType, string contract = null)
+        {
+        }
+
+        /// <inheritdoc />
+        public IDisposable ServiceRegistrationCallback(Type serviceType, string contract, Action<IDisposable> callback) => Disposable.Empty;
+
+        /// <inheritdoc />
+        public virtual void Dispose()
+        {
+        }
+    }
+#pragma warning restore CA1001, CA1063
+}
