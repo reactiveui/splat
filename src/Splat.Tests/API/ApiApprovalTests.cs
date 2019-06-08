cde50335fb3c443f656ddc6acefeafb5963f218b
Glenn Watson <GitHub@glennwatson.net>
2019-06-08 21:06:05 +1000

    Fix

--- src/Splat.Tests/API/ApiApprovalTests.cs
+++ src/Splat.Tests/API/ApiApprovalTests.cs
@@ -4,6 +4,7 @@
 // See the LICENSE file in the project root for full license information.
 
 using System;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
