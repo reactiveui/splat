c078dd6b69c5a2aa7735b4130d243c25012b4aa7
David Vreony <dpvreony@users.noreply.github.com>
2019-07-21 18:25:23 +0100

    autofac lock blocks

--- src/Splat.Autofac/AutofacDependencyResolver.cs
+++ src/Splat.Autofac/AutofacDependencyResolver.cs
@@ -21,6 +21,7 @@ namespace Splat.Autofac
     /// </summary>
     public class AutofacDependencyResolver : IDependencyResolver
     {
+        private readonly object _lockObject = new object();
         private IComponentContext _componentContext;
 
         /// <summary>
@@ -35,39 +36,48 @@ namespace Splat.Autofac
         /// <inheritdoc />
         public virtual object GetService(Type serviceType, string contract = null)
         {
-            try
+            lock (_lockObject)
             {
-                return string.IsNullOrEmpty(contract)
-                    ? _componentContext.Resolve(serviceType)
-                    : _componentContext.ResolveNamed(contract, serviceType);
-            }
-            catch (DependencyResolutionException)
-            {
-                return null;
+                try
+                {
+                    return string.IsNullOrEmpty(contract)
+                        ? _componentContext.Resolve(serviceType)
+                        : _componentContext.ResolveNamed(contract, serviceType);
+                }
+                catch (DependencyResolutionException)
+                {
+                    return null;
+                }
             }
         }
 
         /// <inheritdoc />
         public virtual IEnumerable<object> GetServices(Type serviceType, string contract = null)
         {
-            try
+            lock (_lockObject)
             {
-                var enumerableType = typeof(IEnumerable<>).MakeGenericType(serviceType);
-                object instance = string.IsNullOrEmpty(contract)
-                    ? _componentContext.Resolve(enumerableType)
-                    : _componentContext.ResolveNamed(contract, enumerableType);
-                return ((IEnumerable)instance).Cast<object>();
-            }
-            catch (DependencyResolutionException)
-            {
-                return null;
+                try
+                {
+                    var enumerableType = typeof(IEnumerable<>).MakeGenericType(serviceType);
+                    object instance = string.IsNullOrEmpty(contract)
+                        ? _componentContext.Resolve(enumerableType)
+                        : _componentContext.ResolveNamed(contract, enumerableType);
+                    return ((IEnumerable)instance).Cast<object>();
+                }
+                catch (DependencyResolutionException)
+                {
+                    return null;
+                }
             }
         }
 
         /// <inheritdoc />
         public bool HasRegistration(Type serviceType)
         {
-            return _componentContext.IsRegistered(serviceType);
+            lock (_lockObject)
+            {
+                return _componentContext.IsRegistered(serviceType);
+            }
         }
 
         /// <summary>
@@ -82,17 +92,20 @@ namespace Splat.Autofac
         /// <param name="contract">A optional contract value which will indicates to only generate the value if this contract is specified.</param>
         public virtual void Register(Func<object> factory, Type serviceType, string contract = null)
         {
-            var builder = new ContainerBuilder();
-            if (string.IsNullOrEmpty(contract))
+            lock (_lockObject)
             {
-                builder.Register(x => factory()).As(serviceType).AsImplementedInterfaces();
-            }
-            else
-            {
-                builder.Register(x => factory()).Named(contract, serviceType).AsImplementedInterfaces();
-            }
+                var builder = new ContainerBuilder();
+                if (string.IsNullOrEmpty(contract))
+                {
+                    builder.Register(x => factory()).As(serviceType).AsImplementedInterfaces();
+                }
+                else
+                {
+                    builder.Register(x => factory()).Named(contract, serviceType).AsImplementedInterfaces();
+                }
 
-            builder.Update(_componentContext.ComponentRegistry);
+                builder.Update(_componentContext.ComponentRegistry);
+            }
         }
 
         /// <summary>
@@ -105,76 +118,78 @@ namespace Splat.Autofac
         /// <inheritdoc />
         public virtual void UnregisterCurrent(Type serviceType, string contract = null)
         {
-            // TODO: Thread safety
-            var registrations = _componentContext.ComponentRegistry.Registrations.ToList();
-            var registrationCount = registrations.Count;
-            if (registrationCount < 1)
+            lock (_lockObject)
             {
-                return;
-            }
-
-            var candidatesForRemoval = new List<IComponentRegistration>(registrationCount);
-            var registrationIndex = 0;
-            while (registrationIndex < registrationCount)
-            {
-                var componentRegistration = registrations[registrationIndex];
-
-                var isCandidateForRemoval = GetWhetherServiceIsCandidateForRemoval(
-                    componentRegistration.Services,
-                    serviceType,
-                    contract);
-                if (isCandidateForRemoval)
+                var registrations = _componentContext.ComponentRegistry.Registrations.ToList();
+                var registrationCount = registrations.Count;
+                if (registrationCount < 1)
                 {
-                    registrations.RemoveAt(registrationIndex);
-                    candidatesForRemoval.Add(componentRegistration);
-                    registrationCount--;
+                    return;
                 }
-                else
+
+                var candidatesForRemoval = new List<IComponentRegistration>(registrationCount);
+                var registrationIndex = 0;
+                while (registrationIndex < registrationCount)
                 {
-                    registrationIndex++;
+                    var componentRegistration = registrations[registrationIndex];
+
+                    var isCandidateForRemoval = GetWhetherServiceIsCandidateForRemoval(
+                        componentRegistration.Services,
+                        serviceType,
+                        contract);
+                    if (isCandidateForRemoval)
+                    {
+                        registrations.RemoveAt(registrationIndex);
+                        candidatesForRemoval.Add(componentRegistration);
+                        registrationCount--;
+                    }
+                    else
+                    {
+                        registrationIndex++;
+                    }
                 }
-            }
 
-            if (candidatesForRemoval.Count == 0)
-            {
-                // nothing to remove
-                return;
-            }
-
-            if (candidatesForRemoval.Count > 1)
-            {
-                // need to re-add some registrations
-                var reAdd = candidatesForRemoval.Take(candidatesForRemoval.Count - 1);
-                registrations.AddRange(reAdd);
-
-                /*
-                // check for multi service registration
-                // in future might want to just remove a single service from a component
-                // rather than do the whole component.
-                var lastCandidate = candidatesForRemoval.Last();
-                var lastCandidateRegisteredServices = lastCandidate.Services.ToArray();
-                if (lastCandidateRegisteredServices.Length > 1)
+                if (candidatesForRemoval.Count == 0)
                 {
-                    //
-                    // builder.RegisterType<CallLogger>()
-                    //    .AsSelf()
-                    //    .As<ILogger>()
-                    //    .As<ICallInterceptor>();
-                    var survivingServices = lastCandidateRegisteredServices.Where(s => s.GetType() != serviceType);
-                    var newRegistration = new ComponentRegistration(
-                        lastCandidate.Id,
-                        lastCandidate.Activator,
-                        lastCandidate.Lifetime,
-                        lastCandidate.Sharing,
-                        lastCandidate.Ownership,
-                        survivingServices,
-                        lastCandidate.Metadata);
-                    registrations.Add(newRegistration);
+                    // nothing to remove
+                    return;
                 }
-                */
-            }
 
-            RemoveAndRebuild(registrations);
+                if (candidatesForRemoval.Count > 1)
+                {
+                    // need to re-add some registrations
+                    var reAdd = candidatesForRemoval.Take(candidatesForRemoval.Count - 1);
+                    registrations.AddRange(reAdd);
+
+                    /*
+                    // check for multi service registration
+                    // in future might want to just remove a single service from a component
+                    // rather than do the whole component.
+                    var lastCandidate = candidatesForRemoval.Last();
+                    var lastCandidateRegisteredServices = lastCandidate.Services.ToArray();
+                    if (lastCandidateRegisteredServices.Length > 1)
+                    {
+                        //
+                        // builder.RegisterType<CallLogger>()
+                        //    .AsSelf()
+                        //    .As<ILogger>()
+                        //    .As<ICallInterceptor>();
+                        var survivingServices = lastCandidateRegisteredServices.Where(s => s.GetType() != serviceType);
+                        var newRegistration = new ComponentRegistration(
+                            lastCandidate.Id,
+                            lastCandidate.Activator,
+                            lastCandidate.Lifetime,
+                            lastCandidate.Sharing,
+                            lastCandidate.Ownership,
+                            survivingServices,
+                            lastCandidate.Metadata);
+                        registrations.Add(newRegistration);
+                    }
+                    */
+                }
+
+                RemoveAndRebuild(registrations);
+            }
         }
 
         /// <summary>
@@ -187,18 +202,33 @@ namespace Splat.Autofac
         /// <inheritdoc />
         public virtual void UnregisterAll(Type serviceType, string contract = null)
         {
-            // TODO: Thread safety
-
-            // prevent multiple enumerations
-            var registrations = _componentContext.ComponentRegistry.Registrations.ToList();
-            var registrationCount = registrations.Count;
-            if (registrationCount < 1)
+            lock (_lockObject)
             {
-                return;
-            }
+                // prevent multiple enumerations
+                var registrations = _componentContext.ComponentRegistry.Registrations.ToList();
+                var registrationCount = registrations.Count;
+                if (registrationCount < 1)
+                {
+                    return;
+                }
 
-            if (!string.IsNullOrEmpty(contract))
-            {
+                if (!string.IsNullOrEmpty(contract))
+                {
+                    RemoveAndRebuild(
+                        registrationCount,
+                        registrations,
+                        x => x.Services.All(s =>
+                        {
+                            if (!(s is TypedService typedService))
+                            {
+                                return false;
+                            }
+
+                            return typedService.ServiceType != serviceType || !HasMatchingContract(s, contract);
+                        }));
+                    return;
+                }
+
                 RemoveAndRebuild(
                     registrationCount,
                     registrations,
@@ -209,23 +239,9 @@ namespace Splat.Autofac
                             return false;
                         }
 
-                        return typedService.ServiceType != serviceType || !HasMatchingContract(s, contract);
+                        return typedService.ServiceType != serviceType;
                     }));
-                return;
             }
-
-            RemoveAndRebuild(
-                registrationCount,
-                registrations,
-                x => x.Services.All(s =>
-                {
-                    if (!(s is TypedService typedService))
-                    {
-                        return false;
-                    }
-
-                    return typedService.ServiceType != serviceType;
-                }));
         }
 
         /// <inheritdoc />
@@ -248,9 +264,12 @@ namespace Splat.Autofac
         /// <param name="disposing">Whether or not the instance is disposing.</param>
         protected virtual void Dispose(bool disposing)
         {
-            if (disposing)
+            lock (_lockObject)
             {
-                _componentContext.ComponentRegistry?.Dispose();
+                if (disposing)
+                {
+                    _componentContext.ComponentRegistry?.Dispose();
+                }
             }
         }
 
