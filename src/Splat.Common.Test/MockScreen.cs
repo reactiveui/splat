6dff9521fc10c334b7cb7a0c7a8ca732c6c55104
Chris Pulman <chris.pulman@yahoo.com>
2021-07-25 12:47:54 +0100

    fix for SplatColor Equal and tests for SplatColor
    SplatColor == was looking at how the SplatColor was generated to compare if equal
    Now using ARGB comparision and if all are equal its equal.
    Added KnownName lookup for ARGB which will automatically convert to a KnownName generated Color if it is a match to a KnownColor.

--- src/Splat.Common.Test/MockScreen.cs
+++ src/Splat.Common.Test/MockScreen.cs
@@ -3,12 +3,15 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace Splat.Common.Test
 {
     /// <summary>
     /// An <see cref="IScreen"/> implementation.
     /// </summary>
     /// <seealso cref="IScreen" />
+    [ExcludeFromCodeCoverage]
     public class MockScreen : IScreen
     {
     }
