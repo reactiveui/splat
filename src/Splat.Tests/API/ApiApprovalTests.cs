dec756a2f56acfbce5b59a8e0695c0af269877c1
Simon Cropp <simon.cropp@gmail.com>
2021-10-01 10:50:43 +1000

    apply verify to drawing api test

--- src/Splat.Tests/API/ApiApprovalTests.cs
+++ src/Splat.Tests/API/ApiApprovalTests.cs
@@ -5,11 +5,8 @@
 
 #if !WINDOWS_UWP && !ANDROID
 
-using System;
 using System.Diagnostics.CodeAnalysis;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using PublicApiGenerator;
 using VerifyXunit;
 using Xunit;
 #pragma warning disable SA1615 // Element return value should be documented
@@ -23,27 +20,11 @@ namespace Splat.Tests
     [UsesVerify]
     public class ApiApprovalTests
     {
-        private static readonly Regex _removeCoverletSectionRegex = new(@"^namespace Coverlet\.Core\.Instrumentation\.Tracker.*?^}", RegexOptions.Singleline | RegexOptions.Multiline | RegexOptions.Compiled);
-
         /// <summary>
         /// Tests to make sure the splat project is approved.
         /// </summary>
         [Fact]
-        public Task SplatProject()
-        {
-            var assembly = typeof(AssemblyFinder).Assembly;
-            var generatorOptions = new ApiGeneratorOptions { WhitelistedNamespacePrefixes = new[] { "Splat" } };
-            var apiText = assembly.GeneratePublicApi(generatorOptions);
-            apiText = _removeCoverletSectionRegex.Replace(apiText, string.Empty);
-            return Verifier.Verify(apiText)
-                .UniqueForRuntimeAndVersion()
-                .ScrubEmptyLines()
-                .ScrubLines(l =>
-                    l.StartsWith("[assembly: AssemblyVersion(", StringComparison.InvariantCulture) ||
-                    l.StartsWith("[assembly: AssemblyFileVersion(", StringComparison.InvariantCulture) ||
-                    l.StartsWith("[assembly: AssemblyInformationalVersion(", StringComparison.InvariantCulture) ||
-                    l.StartsWith("[assembly: System.Reflection.AssemblyMetadata(", StringComparison.InvariantCulture));
-        }
+        public Task SplatProject() => typeof(AssemblyFinder).Assembly.CheckApproval2();
     }
 }
 
---
+++ src/Splat.Tests/ApiExtensions.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) 2021 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using PublicApiGenerator;
+using VerifyXunit;
+
+#pragma warning disable SA1615 // Element return value should be documented
+
+namespace Splat.Tests
+{
+    /// <summary>
+    /// A helper for doing API approvals.
+    /// </summary>
+    public static class ApiExtensions
+    {
+        private static readonly Regex _removeCoverletSectionRegex = new(@"^namespace Coverlet\.Core\.Instrumentation\.Tracker.*?^}", RegexOptions.Singleline | RegexOptions.Multiline | RegexOptions.Compiled);
+
+        /// <summary>
+        /// Checks to make sure the API is approved.
+        /// </summary>
+        /// <param name="assembly">The assembly that is being checked.</param>
+        /// <param name="filePath">The caller file path.</param>
+        public static Task CheckApproval2(this Assembly assembly, [CallerFilePath] string filePath = "")
+        {
+            var generatorOptions = new ApiGeneratorOptions { WhitelistedNamespacePrefixes = new[] { "Splat" } };
+            var apiText = assembly.GeneratePublicApi(generatorOptions);
+            apiText = _removeCoverletSectionRegex.Replace(apiText, string.Empty);
+            return Verifier.Verify(apiText, null, filePath)
+                .UniqueForRuntimeAndVersion()
+                .ScrubEmptyLines()
+                .ScrubLines(l =>
+                    l.StartsWith("[assembly: AssemblyVersion(", StringComparison.InvariantCulture) ||
+                    l.StartsWith("[assembly: AssemblyFileVersion(", StringComparison.InvariantCulture) ||
+                    l.StartsWith("[assembly: AssemblyInformationalVersion(", StringComparison.InvariantCulture) ||
+                    l.StartsWith("[assembly: System.Reflection.AssemblyMetadata(", StringComparison.InvariantCulture));
+        }
+    }
+}
