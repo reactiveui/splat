cf1cfd441c0ef400498e507b1d0658391b203673
Glenn <5834289+glennawatson@users.noreply.github.com>
2021-10-31 13:22:46 +1100

    Update ApiApprovalTests.cs

--- src/Splat.Tests/API/ApiApprovalTests.cs
+++ src/Splat.Tests/API/ApiApprovalTests.cs
@@ -15,7 +15,6 @@ namespace Splat.Tests
     /// <summary>
     /// Tests to make sure that the API matches the approved ones.
     /// </summary>
-    /// <returns>A task to monitor the usage.<returns>
     [ExcludeFromCodeCoverage]
     [UsesVerify]
     public class ApiApprovalTests
@@ -23,6 +22,7 @@ namespace Splat.Tests
         /// <summary>
         /// Tests to make sure the splat project is approved.
         /// </summary>
+        /// <returns>A task to monitor the usage.<returns>
         [Fact]
         public Task SplatProject() => typeof(AssemblyFinder).Assembly.CheckApproval();
     }
