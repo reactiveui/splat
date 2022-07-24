211f99d4e9d16da600a44120c3d4e1ed876a1c52
Adam Radocz <adam.radocz@gmail.com>
2022-07-24 15:32:53 +0200

    #938 Add default Run and Test mode. (#939)
    Co-authored-by: Chris Pulman <chris.pulman@yahoo.com>

---
+++ src/Splat.Tests/ModeDetection/ModeTests.cs
@@ -0,0 +1,43 @@
+﻿using Splat.ModeDetection;
+using Xunit;
+
+namespace Splat.Tests.ModeDetection
+{
+    /// <summary>
+    /// Unit tests for the <see cref="Mode"/> class.
+    /// </summary>
+    public class ModeTests
+    {
+        /// <summary>
+        /// Tests the <see cref="Mode.Run"/> mode.
+        /// </summary>
+        [Fact]
+        public void RunModeTest()
+        {
+            // Arrange
+            ModeDetector.OverrideModeDetector(Mode.Run);
+
+            // Act
+            var inUnitTestRunner = ModeDetector.InUnitTestRunner();
+
+            // Assert
+            Assert.False(inUnitTestRunner);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="Mode.Test"/> mode.
+        /// </summary>
+        [Fact]
+        public void TestModeTest()
+        {
+            // Arrange
+            ModeDetector.OverrideModeDetector(Mode.Test);
+
+            // Act
+            var inUnitTestRunner = ModeDetector.InUnitTestRunner();
+
+            // Assert
+            Assert.True(inUnitTestRunner);
+        }
+    }
+}
---
+++ src/Splat/ModeDetection/Mode.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) 2021 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+namespace Splat.ModeDetection
+{
+    /// <summary>
+    /// The default implementation of the <see cref="Run"/> and <see cref="Test"/> mode.
+    /// </summary>
+    public sealed class Mode : IModeDetector
+    {
+        /// <summary>
+        /// The default implementation of the run mode.
+        /// </summary>
+        public static readonly Mode Run = new(false);
+
+        /// <summary>
+        /// The default implementation of the test mode.
+        /// </summary>
+        public static readonly Mode Test = new(true);
+
+        private readonly bool _inUnitTestRunner;
+
+        private Mode(bool inUnitTestRunner) => _inUnitTestRunner = inUnitTestRunner;
+
+        /// <inheritdoc/>
+        public bool? InUnitTestRunner() => _inUnitTestRunner;
+    }
+}
