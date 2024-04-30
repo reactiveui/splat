283951022d1022c93da8f85e991cb459dac5268d
Chris Pulman <chris.pulman@yahoo.com>
2024-04-30 23:05:49 +0100

    Housekeeping: Update Copyright

--- src/Splat/Logging/DebugLogger.cs
+++ src/Splat/Logging/DebugLogger.cs
@@ -1,8 +1,7 @@
-﻿// Copyright (c) 2023 .NET Foundation and Contributors. All rights reserved.
+﻿// Copyright (c) 2024 .NET Foundation and Contributors. All rights reserved.
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
-
 // We need to define the DEBUG symbol because we want the logger
 // to work even when this package is compiled on release. Otherwise,
 // the call to Debug.WriteLine will not be in the release binary
