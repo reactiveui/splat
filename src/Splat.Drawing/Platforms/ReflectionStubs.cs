db0a52b097ed005bfdf0e4ac37a79d7ed0b228a4
Chris Pulman <chris.pulman@yahoo.com>
2025-01-28 21:57:54 +0000

    Add further AOT Markup Update Packages (#1264)

--- src/Splat.Drawing/Platforms/ReflectionStubs.cs
+++ src/Splat.Drawing/Platforms/ReflectionStubs.cs
@@ -3,12 +3,17 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 
 namespace Splat;
 
 internal static class ReflectionStubs
 {
+#if NET6_0_OR_GREATER
+    [RequiresUnreferencedCode("Type.GetField() may be trimmed.")]
+    [RequiresDynamicCode("Type.GetField() may be trimmed.")]
+#endif
     public static FieldInfo? GetField(this Type value, string name, BindingFlags flags = default)
     {
         var ti = value.GetTypeInfo();
@@ -18,6 +23,10 @@ internal static class ReflectionStubs
             : ti.BaseType.GetField(name, flags);
     }
 
+#if NET6_0_OR_GREATER
+    [RequiresUnreferencedCode("Type.GetMethod() may be trimmed.")]
+    [RequiresDynamicCode("Type.GetMethod() may be trimmed.")]
+#endif
     public static MethodInfo? GetMethod(this Type value, string name, BindingFlags flags = default)
     {
         var ti = value.GetTypeInfo();
@@ -27,6 +36,10 @@ internal static class ReflectionStubs
             : ti.BaseType.GetMethod(name, flags);
     }
 
+#if NET6_0_OR_GREATER
+    [RequiresUnreferencedCode("Type.GetProperty() may be trimmed.")]
+    [RequiresDynamicCode("Type.GetProperty() may be trimmed.")]
+#endif
     public static PropertyInfo? GetProperty(this Type value, string name, BindingFlags flags = default)
     {
         var ti = value.GetTypeInfo();
@@ -36,6 +49,10 @@ internal static class ReflectionStubs
             : ti.BaseType.GetProperty(name, flags);
     }
 
+#if NET6_0_OR_GREATER
+    [RequiresUnreferencedCode("Type.GetEvent() may be trimmed.")]
+    [RequiresDynamicCode("Type.GetEvent() may be trimmed.")]
+#endif
     public static EventInfo? GetEvent(this Type value, string name, BindingFlags flags = default)
     {
         var ti = value.GetTypeInfo();
@@ -45,10 +62,22 @@ internal static class ReflectionStubs
             : ti.BaseType.GetEvent(name, flags);
     }
 
+#if NET6_0_OR_GREATER
+    [RequiresUnreferencedCode("Type.GetProperties() may be trimmed.")]
+    [RequiresDynamicCode("Type.GetProperties() may be trimmed.")]
+#endif
     public static IEnumerable<PropertyInfo> GetProperties(this Type value) => value.GetTypeInfo().DeclaredProperties;
 
+#if NET6_0_OR_GREATER
+    [RequiresUnreferencedCode("Type.GetFields() may be trimmed.")]
+    [RequiresDynamicCode("Type.GetFields() may be trimmed.")]
+#endif
     public static IEnumerable<FieldInfo> GetFields(this Type value) => value.GetTypeInfo().DeclaredFields;
 
+#if NET6_0_OR_GREATER
+    [RequiresUnreferencedCode("Type.GetMethod() may be trimmed.")]
+    [RequiresDynamicCode("Type.GetMethod() may be trimmed.")]
+#endif
     public static MethodInfo? GetMethod(this Type value, string methodName, Type[] paramTypes, BindingFlags flags = default)
     {
         var ti = value.GetTypeInfo();
@@ -60,6 +89,10 @@ internal static class ReflectionStubs
             : ti.BaseType.GetMethod(methodName, paramTypes, flags);
     }
 
+#if NET6_0_OR_GREATER
+    [RequiresUnreferencedCode("Type.GetMethods() may be trimmed.")]
+    [RequiresDynamicCode("Type.GetMethods() may be trimmed.")]
+#endif
     public static IEnumerable<MethodInfo> GetMethods(this Type value) => value.GetTypeInfo().DeclaredMethods;
 
     public static IEnumerable<object> GetCustomAttributes(this Type value, Type attributeType, bool inherit) => value.GetTypeInfo().GetCustomAttributes(attributeType, inherit);
