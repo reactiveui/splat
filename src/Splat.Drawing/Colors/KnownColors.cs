8a57f47325c33e1e9f1d179139dbb9b4ef95929d
Chris Pulman <chris.pulman@yahoo.com>
2026-01-13 01:57:21 +0000

    chore: Update copyright year to 2026 across source files (#1474)

--- src/Splat.Drawing/Colors/KnownColors.cs
+++ src/Splat.Drawing/Colors/KnownColors.cs
@@ -1,7 +1,8 @@
-﻿// Copyright (c) 2025 ReactiveUI. All rights reserved.
+﻿// Copyright (c) 2026 ReactiveUI. All rights reserved.
 // Licensed to ReactiveUI under one or more agreements.
 // ReactiveUI licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
+
 using System.Runtime.CompilerServices;
 
 namespace Splat;
